34b7ee148964bb50d30b150c9c1875b5b768b4ad
FranTorquato <frantorquato@gmail.com>
2026-03-11 20:53:36 -0300

    Primeira versão do projeto

---
+++ mineralis/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready() -> void:
+	pass 
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_btn_exit_pressed():
+	get_tree().quit() 
+
+func _on_btn_new_game_mouse_entered():
+	# Faz o Corvan começar a marretar quando o mouse entra no botão 
+	$AnimatedSprite2D.play("marretando")
+
+func _on_btn_new_game_mouse_exited():
+	# Faz o Corvan voltar a ficar parado quando o mouse sai 
+	$AnimatedSprite2D.play("idle")
